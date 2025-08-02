166007511de5a17c3f1c20cf66181470185435fc
Anastasiia Dymarchuk <anasdym@gmail.com>
2025-08-02 11:55:52 +0200

    In process

--- todo (file)/todo.py
+++ todo (file)/todo.py
@@ -1,26 +1,36 @@
 # zadanie https://mhyla.com/jica-python6/
+from operator import indexOf
 
-zadania = []
+zadania = {}
 
 def dodaj_zadanie():
    global zadanie
    tytul = input("Napisz tytuł zadania: ")
    priorytet = input("Napisz priorytet zadania: ")
    zadanie = (tytul, priorytet)
-   zadania.append(zadanie)
-
+   zadania[zadanie] = False
 
 def pokaz_zadania():
-   for i in zadania:
-       print(i)
-
+   for i, status in zadania.items():
+       if status:
+           print(indexOf(zadania, i) + i, " [✔]")
+       else:
+           print(indexOf(zadania, i) + i," [ ]")
 
 while True:
-   dodaj_zadanie()
+    dodaj_zadanie()
 
+    komenda = input("\nChcesz..\ndodać kolejne? - napisz TAK\noznaczyć jako wykonane - napisz ZROB\nusunąć zadanie? - napisz USUŃ")
+    komenda = komenda.lower()
+    match komenda:
+        case "tak":
+            dodaj_zadanie()
+        case "zrob":
+            pokaz_zadania()
 
-   kolejne = input("Chcesz dodać kolejne? ")
-   if kolejne.lower() != "tak":
-       pokaz_zadania()
-       break
+        case "usuń":
+            pokaz_zadania()
 
+        case _:
+            pokaz_zadania()
+            break
