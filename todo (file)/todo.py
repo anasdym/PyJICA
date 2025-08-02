8517809eaf7ab3b22d03eb5f0945c60912d4eb79
anasdym <anasdym@gmail.com>
2025-08-02 13:35:19 +0200

    enumerate

--- todo (file)/todo.py
+++ todo (file)/todo.py
@@ -1,36 +1,36 @@
-# zadanie https://mhyla.com/jica-python6/
-from operator import indexOf
-
 zadania = {}
 
+
 def dodaj_zadanie():
-   global zadanie
-   tytul = input("Napisz tytuł zadania: ")
-   priorytet = input("Napisz priorytet zadania: ")
-   zadanie = (tytul, priorytet)
-   zadania[zadanie] = False
+    global zadanie
+    tytul = input("Napisz tytuł zadania: ")
+    priorytet = input("Napisz priorytet zadania: ")
+    zadanie = (tytul, priorytet)
+    zadania[zadanie] = False
 
 def pokaz_zadania():
-   for i, status in zadania.items():
-       if status:
-           print(indexOf(zadania, i) + i, " [✔]")
-       else:
-           print(indexOf(zadania, i) + i," [ ]")
+    for i, (zadanie, status) in enumerate(zadania.items()):
+        if status:
+            print(f"{i + 1}. {zadanie} [✔]")
+        else:
+            print(f"{i + 1}. {zadanie} [ ]")
 
 while True:
     dodaj_zadanie()
 
-    komenda = input("\nChcesz..\ndodać kolejne? - napisz TAK\noznaczyć jako wykonane - napisz ZROB\nusunąć zadanie? - napisz USUŃ")
+    komenda = input(
+        "\nChcesz..\ndodać kolejne? - napisz TAK\noznaczyć jako wykonane - napisz ZROB\nusunąć zadanie? - napisz USUŃ")
     komenda = komenda.lower()
+
     match komenda:
         case "tak":
             dodaj_zadanie()
         case "zrob":
             pokaz_zadania()
-
-        case "usuń":
+        case "usun":
             pokaz_zadania()
-
         case _:
             pokaz_zadania()
             break
+
+# zadanie https://mhyla.com/jica-python6/
