81808638a53b99ff3c4bd35ff30f2b25c3c4fcbb
anasdym <anasdym@gmail.com>
2025-08-02 14:01:59 +0200

    Dodano 2 nowe funkcii

--- todo (file)/todo.py
+++ todo (file)/todo.py
@@ -1,6 +1,5 @@
 zadania = {}
 
-
 def dodaj_zadanie():
     global zadanie
     tytul = input("Napisz tytuł zadania: ")
@@ -9,28 +8,43 @@ def dodaj_zadanie():
     zadania[zadanie] = False
 
 def pokaz_zadania():
-    for i, (zadanie, status) in enumerate(zadania.items()):
+    for i, (zadanie, status) in enumerate(zadania.items()): #wraca z numerem slownika
         if status:
             print(f"{i + 1}. {zadanie} [✔]")
         else:
             print(f"{i + 1}. {zadanie} [ ]")
 
-while True:
-    dodaj_zadanie()
+def wykonaj_zadanie():
+    n = int(input("Które zadanie jest już wykonane? - napisz numer"))
+    #zaprogramowac oznaczenie
+    pokaz_zadania()
 
-    komenda = input(
-        "\nChcesz..\ndodać kolejne? - napisz TAK\noznaczyć jako wykonane - napisz ZROB\nusunąć zadanie? - napisz USUŃ")
-    komenda = komenda.lower()
+def usun_zadanie():
+    n = int(input("Które zadanie usunąć? - napisz numer"))
+    # zaprogramowac usuwanie
+    pokaz_zadania()
 
-    match komenda:
-        case "tak":
-            dodaj_zadanie()
-        case "zrob":
-            pokaz_zadania()
-        case "usun":
-            pokaz_zadania()
-        case _:
-            pokaz_zadania()
-            break
+def main ():
+        dodaj_zadanie()
+
+        while True:
+            komenda = input("\nChcesz..\ndodać kolejne? - napisz TAK\noznaczyć jako wykonane - napisz ZROB\nusunąć zadanie? - napisz USUN")
+            komenda = komenda.lower()
+
+            match komenda:
+                case "tak":
+                    dodaj_zadanie()
+                case "zrob":
+                    pokaz_zadania()
+                    wykonaj_zadanie()
+                case "usun":
+                    pokaz_zadania()
+                    usun_zadanie()
+                case _:
+                    pokaz_zadania()
+                    break
+
+if __name__ == "__main__":
+   main()
 
 # zadanie https://mhyla.com/jica-python6/
