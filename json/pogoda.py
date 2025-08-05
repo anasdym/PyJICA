197297dfafd48ed85f203a5639df56edea435bbd
anasdym <anasdym@gmail.com>
2025-08-05 09:55:41 +0200

    Pogoda created, works on finding coordinates and forecast

--- json/pogoda.py
+++ json/pogoda.py
@@ -3,17 +3,44 @@ from logging import exception
 import requests
 from requests import Response
 
-def getData(city):
+def getCoord(city):
 
-    response = response = requests.get("https://geocoding-api.open-meteo.com/v1/search?name=<miasto>", {'name': city}, timeout=10)
-    response.raise_for_status()
-    data = response.json()
+    try:
+        response = requests.get("https://geocoding-api.open-meteo.com/v1/search?name=<miasto>", params = {'name': city}, timeout=10)
+        response.raise_for_status()
+        data = response.json()
 
-    if 'results' in data and data['results']:
-        first_result = data['results'][0]
-        latitude = first_result.get('latitude')
-        longitude = first_result.get('longitude')
-        return latitude, longitude
+        if 'results' in data and data['results']:
+            first_result = data['results'][0]
+            latitude = first_result.get('latitude')
+            longitude = first_result.get('longitude')
+            return latitude, longitude
+
+    except Exception as err:
+        print(f'Wystąpił błąd: {err}')
+
+def getWheather(latitude, longitude):
+
+    params = {
+        'latitude': latitude,
+        'longitude': longitude,
+        'current_weather': 'true'
+    }
+
+    try:
+        response = requests.get("https://api.open-meteo.com/v1/forecast", params = params, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'current_weather' in data:
+            current_weather = data['current_weather']
+            temperature = current_weather.get('temperature')
+            windspeed = current_weather.get('windspeed')
+
+            return temperature, windspeed
+
+    except Exception as err:
+        print(f'Wystąpił błąd: {err}')
 
 def getCities():
 
@@ -37,10 +64,16 @@ def main():
     cities = getCities()
 
     for city in cities:
-        cityData = getData(city)
-        if cityData:
-            latitude, longitude = cityData
+        city_coord = getCoord(city)
+        if city_coord:
+            latitude, longitude = city_coord
             print(city, latitude, longitude)
+
+            city_temp, city_wind = getWheather(latitude, longitude)
+            if city_temp:
+                print(f"{city_temp}°C, {city_wind} km/h")
+            else:
+                print(f"[WARN] Brak danych pogodowych: {city}")
         else:
             print(f"[WARN] Nie znaleziono miasta: {city}")
 
