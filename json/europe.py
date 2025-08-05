0e8af3a7543b9b7637ce8a4d6451a3404816e83f
anasdym <anasdym@gmail.com>
2025-08-05 07:43:19 +0200

    Europe complited

--- json/europe.py
+++ json/europe.py
@@ -1,41 +1,45 @@
 import json
 import requests
 
+europe = []
+
+def getData():
+    try:
+        response = requests.get("https://restcountries.com/v3.1/all?fields=name,capital,population,region", timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+    except requests.exceptions.ConnectionError as err:
+        print(f"Brak internetu: {err}")
+
+    except json.JSONDecodeError:
+        print("Bład: puste odpowiedzi z API.")
+
+    return data
+
 def main():
 
-    europe = []
+    data = getData()
 
-    response = requests.get("https://restcountries.com/v3.1/all?fields=name,capital,population,region", timeout=10)
-    data = response.json()
-    if response.status_code == 200:
+    for country in data:
+        if country["region"] == "Europe":
+            name = country["name"]["common"]
+            if country["capital"]:
+                capital = country["capital"][0]
+            else:
+                capital = "-"
 
-        for country in data:
-            if country["region"] == "Europe":
-                name = country["name"]["common"]
-                if country["capital"]:
-                    capital = country["capital"][0]
-                else:
-                    capital = "-"
+            population = country["population"]
 
-                population = country["population"]
-
-                tuple = (name, capital, population)
-                europe.append((tuple))
-
-        europe.sort(key=lambda x: x[2], reverse=True) # sortowanie po 2+1 elemencie tupli
-        print(len(europe), " krajów jest w Europie")
-
-        print("TOP-10:")
-        for i in range(0,10):
-            print(f"{i+1}. {europe[i][0]} - capital: {europe[i][1]} - population: {europe[i][2]:,}")
-
-        for i in europe:
-            suma = 0
-            suma = suma + int(europe[i][2])
-        print(suma)
-
-    else:
-        print("Error ", response.status_code)
+            tuple = (name, capital, population)
+            europe.append((tuple))
+    europe.sort(key=lambda x: x[2], reverse=True)  # sortowanie po 2+1 elemencie tupli
+    print(len(europe), " krajów jest w Europie")
+    print("\nTOP-10 of population:")
+    for i in range(0, 10):
+        print(f"{i + 1}. {europe[i][0]} - capital: {europe[i][1]} - population: {europe[i][2]:,}")
+    europePop = sum(item[2] for item in europe)
+    print(f"\nSuma populacji wszystkich krajów w europe: {europePop:,}")
 
 if __name__ == '__main__':
     main()
